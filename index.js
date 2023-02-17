e76b7d53a1b1ca7224691af0001ca7efed315f9c
miguelsf12 <87654640+miguelsf12@users.noreply.github.com>
2023-02-17 19:39:32 -0300

    finalizado

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
 let inputTarefas = document.querySelector('#idtask') // INPUT ONDE ENTRA AS TAREFAS
 let campTasks = document.querySelector('.campTasks') // ELEMENTO PAI ONDE FICARÁ AS TASKS
-let tarefa = document.querySelector('.task') // div que contem toda a tarefa botao e texto
 
-
+// INSERINDO TASKS
 
 function inserirTask() {
     if (inputTarefas.value == "") {
         alert('Insira alguma tarefa abaixo para prosseguir.')
     } else {
         campTasks.innerHTML += `
-    <div class="task">
-        <li class="tarefa">${inputTarefas.value.toUpperCase()}</li>
-        <div class="statusTask">
-            <box-icon onclick="apagarConcluir()" name='check-square' type='solid' color='#00ff00' ></box-icon>
-            <box-icon onclick="apagarConcluir()" name='x-square' type='solid' color='#ff0000' ></box-icon>
-        </div>
-    </div>`
+        <div class="task">
+            <li class="tarefa">${inputTarefas.value.toUpperCase()}</li>
+            <div class="statusTask">
+                <box-icon class="btnFinish" name='check-square' type='solid' color='#00ff00' ></box-icon>
+                <box-icon class="btnDel" name='x-square' type='solid' color='#ff0000' ></box-icon>
+            </div>
+        </div>`
 
         inputTarefas.value = ""
     }
-
-
 }
 
-// criar funcao para apagar e marcar como concluido
+// APAGANDO TARERFAS & MARCANDO COMO CONCLUIDA
+
+document.addEventListener("click", (e) => {
+    const elementoAlvo = e.target
+    const elementoProx = elementoAlvo.closest(".task") // minha div .task
+
+
+    if (elementoAlvo.classList.contains("btnDel")) {
+        setTimeout(() => {
+            elementoProx.remove()
+        }, 200);
+    }
+
+    if (elementoAlvo.classList.contains('btnFinish')) {
+        elementoProx.classList.toggle('finish')
+        setTimeout(() => {
+            elementoProx.remove()
+        }, 500);
+    }
+
+})
